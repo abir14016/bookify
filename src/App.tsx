3226838885869e4460bd4e9bc39641e4730e08fc
abir14016 <abir14016@gmail.com>
2023-08-06 06:32:35 +0600

    [DONE] setup daisyUI and generate custom theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 function App() {
   return (
-    <h2 className="text-3xl font-bold text-emerald-700 text-center">Bookify</h2>
+    <>
+      <h2 className="text-3xl font-bold text-accent text-center">Bookify</h2>
+      <button className="btn btn-primary">Hello</button>
+      <button className="btn btn-secondary">Hello</button>
+      <button className="btn btn-accent">Hello</button>
+      <button className="btn btn-warning">Hello</button>
+      <button className="btn btn-success">Hello</button>
+      <button className="btn btn-error">Hello</button>
+      <button className="btn">Hello</button>
+    </>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,27 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        bookifyTheme: {
+          "primary": "#0FCFEC",
+          "secondary": "#19D3AE",
+          "accent": "#3A4256",
+          "neutral": "#3d4451",
+          "base-100": "#1d232a",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
 
