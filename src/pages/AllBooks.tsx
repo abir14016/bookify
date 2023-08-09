7f43f01f05961fb28cb1a5bcb69aaceb4f896c7b
abir14016 <abir14016@gmail.com>
2023-08-09 10:27:22 +0600

    [DONE] implement filter by genre

--- src/pages/AllBooks.tsx
+++ src/pages/AllBooks.tsx
@@ -5,25 +5,48 @@ import { useGetAllBooksQuery } from "../redux/api/apiSlice";
 import { IBook } from "../types/globalTypes";
 
 const AllBooks = () => {
+  const [showPlaceholder, setShowPlaceholder] = useState(true);
   const [searchText, setSearchText] = useState("");
+  const [selectedGenre, setSelectedGenre] = useState(""); // State for selected genre
   const { data, isLoading } = useGetAllBooksQuery(undefined);
   let books: IBook[] = [];
   if (!isLoading) {
     books = data.data;
   }
 
+  // Retrieve unique genres from the book data
+  const allGenres = [...new Set(books.map((book) => book.genre))];
+
   // Handler for input change
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchText(event.target.value);
   };
 
+  // Handler for genre selection
+  const handleGenreSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedGenre(event.target.value);
+  };
+
   // Filtering books based on the search text
-  const filteredBooks = books.filter(
+  // Filtering books based on the search text
+  const searchFilteredBooks = books.filter(
     (book) =>
       book.title.toLowerCase().includes(searchText.toLowerCase()) ||
       book.author.toLowerCase().includes(searchText.toLowerCase()) ||
       book.genre.toLowerCase().includes(searchText.toLowerCase())
   );
+
+  // Filtering books based on the selected genre
+  const genreFilteredBooks = books.filter(
+    (book) =>
+      selectedGenre === "" ||
+      book.genre.toLowerCase() === selectedGenre.toLowerCase()
+  );
+
+  // Combining the results of search and genre filtering
+  const combinedFilteredBooks = searchText
+    ? searchFilteredBooks
+    : genreFilteredBooks;
   return (
     <div>
       <div className="px-2 md:px-8 lg:px-10 xl:px-12">
@@ -32,6 +55,32 @@ const AllBooks = () => {
             <h2 className="mr-3">All Books</h2>
             <hr className="hidden md:block lg:block xl:block  w-48 h-1 bg-gradient-to-r from-primary to-secondary rounded border-0" />
           </div>
+        </div>
+
+        <div className="flex justify-between items-center">
+          <div className="flex justify-start">
+            <div>
+              <label className="label">
+                <span className="label-text-alt">Filter by genre</span>
+              </label>
+              <select
+                value={selectedGenre}
+                onChange={handleGenreSelect}
+                className="select select-bordered select-primary select-sm"
+                onFocus={() => setShowPlaceholder(false)}
+                onBlur={() => setShowPlaceholder(!selectedGenre)}
+              >
+                {showPlaceholder && <option value="">Select a genre</option>}
+                <option value="">All</option>
+                {allGenres.map((genre) => (
+                  <option key={genre} value={genre}>
+                    {genre}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+
           <div>
             <input
               value={searchText}
@@ -42,12 +91,13 @@ const AllBooks = () => {
             />
           </div>
         </div>
-        <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-10 my-2 md:my-4 lg:my-6">
+
+        <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-10 my-2 md:my-10 lg:my-12 xl:my-16">
           {isLoading
             ? Array.from({ length: 20 }).map((_, index) => (
                 <BookSkeleton key={index} />
               ))
-            : filteredBooks.map((book) => (
+            : combinedFilteredBooks.map((book) => (
                 <BookCard book={book} key={book._id} />
               ))}
         </div>
