3eb205c1ac43eaf1d89c83706b8b20d0217d3d41
abir14016 <abir14016@gmail.com>
2023-08-16 20:46:57 +0600

    [DONE] resolved page not found issue after reloading in live link of netlify

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      // Copy the _redirects file to the root of the build directory
+      output: {
+        assetFileNames: "[name][extname]",
+      },
+    },
+  },
   server: {
     port: 3000,
   },
